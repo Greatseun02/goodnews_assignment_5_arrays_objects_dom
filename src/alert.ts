a6190bb0118482742a89aa919c5b22bf7fb89aff
greatseun02 <goodnews.adewole9@gmail.com>
2025-05-18 12:21:11 +0100

    done with no.5

---
+++ src/alert.ts
@@ -0,0 +1,33 @@
+const button = document.querySelectorAll(".button");
+const alerts = document.querySelectorAll(".label");
+const cancels = document.querySelectorAll(".cancel");
+
+button.forEach((btn)=>{
+    btn.addEventListener("click", (e)=>{
+        
+        alerts.forEach((alert)=>{
+            if(btn.classList.contains("success") && alert.id ==="success"){
+                alert.classList.remove("hidden");
+            }else if(btn.classList.contains("error") && alert.id ==="error"){
+                alert.classList.remove("hidden");
+            }else if(btn.classList.contains("warning") && alert.id ==="warning"){
+                alert.classList.remove("hidden");
+            }
+        })
+        
+    })
+})
+
+cancels.forEach(cancel=>{
+    cancel.addEventListener("click", (e)=>{
+        alerts.forEach((alert)=>{
+            if(cancel.parentElement?.id === "success" && alert.id ==="success"){
+                alert.classList.add("hidden");
+            }else if(cancel.parentElement?.id === "error" && alert.id ==="error"){
+                alert.classList.add("hidden");
+            }else if(cancel.parentElement?.id === "warning" && alert.id ==="warning"){
+                alert.classList.add("hidden");
+            }
+        })
+    })
+})
